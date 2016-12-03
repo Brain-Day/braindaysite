65fc828c9e7d8664f6d1f2c703538dd78ca1fc21
Edward Guo <edward.h.guo@gmail.com>
2016-12-02 19:59:58 -0800

    folders

---
+++ src/app/404/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  template: `
+    <div>
+      <h1>Page Not Found</h1>
+    </div>
+  `
+})
+export class NotFoundComponent { }
